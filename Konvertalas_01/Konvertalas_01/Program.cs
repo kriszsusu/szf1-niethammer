c2a9fa732cf0a906ecb6fb760e98f0428b729f73
Krisz <145597465+kriszsusu@users.noreply.github.com>
2024-09-25 13:19:05 +0200

    a

--- Konvertalas_01/Konvertalas_01/Program.cs
+++ Konvertalas_01/Konvertalas_01/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int db = 125;
+            
+            double egy_db_ara = 230.5;
+
+            int osszesen1 = db * Convert.ToInt32(egy_db_ara);
+            double osszesen2 = Convert.ToDouble(db) * egy_db_ara;
+
+            Console.WriteLine($"Osszesen1 - egesz szamkent: {osszesen1}");
+            Console.WriteLine($"Osszesen2 - dupla szamkent: {osszesen2}");
+
+            byte x = 128;       //  0..255
+            byte y = 131;
+
+            byte z = (byte)(x + y);
+
+            int nagy_szam = 0x7fffffff;  // Van maximális értéke: 2147483647
+            Console.WriteLine($"Nagyon nagy szam: {nagy_szam}");
+
+            char betu = (char)(97 - 32);
+            Console.WriteLine($"Karakter: {betu}");
+
+            string szoveg = "Ez a hosszú szöveg";
         }
     }
 }
