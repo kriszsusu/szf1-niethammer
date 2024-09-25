2cca2d1e9180f907ee0d26b697a24689a8ee9a2d
Krisz <145597465+kriszsusu@users.noreply.github.com>
2024-09-25 13:59:22 +0200

    elso ket feladat lul

---
+++ Feladat1/Feladat1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Feladat1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Feladat:
+            // Egész szám -> 150
+            // Tört szám  -> 200.25
+            // Mennyi a szorzata?
+
+            int egesz   = 150;
+            double tort = 200.25;
+            
+            double szorzat = Convert.ToInt32(egesz) * tort;
+            Console.WriteLine($"A ket szam szorzata: {szorzat}");
+
+            // Feladat 2:
+            // Képernyőre kiíratni a-tól z-ig a betűket egymás mellé
+
+            for (byte b = 97; b <= 122; b++)
+            {
+                Console.Write((char)b);
+            }    
+        }
+    }
+}
