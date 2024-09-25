89c5ffb8d2df9e1403e95b5c664daafd7a96112f
Krisz <145597465+kriszsusu@users.noreply.github.com>
2024-09-25 12:46:52 +0200

    Add files via upload

---
+++ Konvertalas_01/Konvertalas_01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Konvertalas_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
