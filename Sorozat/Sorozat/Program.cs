6f8ea057a345158d007750ec6a120425008c0799
Chlebik Krisztofer Benjámin <145597465+kriszsusu@users.noreply.github.com>
2024-10-09 13:50:17 +0200

    Add files via upload

---
+++ Sorozat/Sorozat/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sorozat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nem adott meg két paramétert. (String, Int)");
+            }
+            else if (args.Length > 1)
+            {
+                string szoveg = args[0];
+                int times = Convert.ToInt32(args[1]);
+
+                for ( int i = 0; i < times; i++)
+                {
+                    Console.Write(szoveg);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nem adott meg elég paramétert. (String, Int)");
+            }
+        }
+    }
+}
