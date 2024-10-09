b6a88710335301336c9a19e8d04bb962ec9ec649
Chlebik Krisztofer Benjámin <145597465+kriszsusu@users.noreply.github.com>
2024-10-09 13:32:54 +0200

    Add files via upload

---
+++ calc/calc/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 1)
+            {
+                int x = Convert.ToInt32(args[0]);
+                char op = Convert.ToChar(args[1]);
+                int y = Convert.ToInt32(args[2]);
+
+                switch(op)
+                {
+                    case '+':
+                        Console.WriteLine($"Eredmény: {x + y}");
+                        break;
+
+                    case '-':
+                        Console.WriteLine($"Eredmény: {x - y}");
+                        break;
+
+                    case '*':
+                        Console.WriteLine($"Eredmény: {x * y}");
+                        break;
+
+                    case '/':
+                        Console.WriteLine($"Eredmény: {x / y}");
+                        break;
+                }
+
+            } else
+            {
+                Console.WriteLine("Nincs elég paraméter megadva!");
+            }
+        }
+    }
+}
