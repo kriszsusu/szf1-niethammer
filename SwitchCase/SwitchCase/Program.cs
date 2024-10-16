9a064e723034dc59135c3920c993a1b6b11e1a25
Chlebik Krisztofer <145597465+kriszsusu@users.noreply.github.com>
2024-10-16 13:11:55 +0200

    Add files via upload

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] jegyek = { 5, 4, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1 };
+            int sum = 0;
+
+            for (int i = 0; i < jegyek.Length; i++) {
+                sum += jegyek[i];
+            }
+
+            double atlag = Convert.ToDouble(sum) / jegyek.Length;
+
+            switch (atlag)
+            {
+                case > 4.5:
+                    Console.WriteLine("jeles");
+                    break;
+                case > 3.5:
+                    Console.WriteLine("jó");
+                    break;
+                case > 2.5:
+                    Console.WriteLine("közepes");
+                    break;
+                case > 1.5:
+                    Console.WriteLine("elégséges");
+                    break;
+                default:
+                    Console.WriteLine("elégtelen");
+                    break;
+            }
+        }
+    }
+}
---
+++ SwitchCase02/SwitchCase02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SwitchCase02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char betu = (char)Console.Read();
+
+            switch (betu)
+            {
+                case 'A':
+                case 'a':
+                    Console.WriteLine("BALRA");
+                    break;
+                case 'D':
+                case 'd':
+                    Console.WriteLine("JOBBRA");
+                    break;
+                case 'W':
+                case 'w':
+                    Console.WriteLine("FELFELE");
+                    break;
+                case 'S':
+                case 's':
+                    Console.WriteLine("LEFELE");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
